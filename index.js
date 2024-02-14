21fe873d4bda0159fa9e5c8fdc6affd6da9ce887
BuildTools <unconfigured@null.spigotmc.org>
2024-02-14 16:55:15 +0100

    Cli Tool support 1.0

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+#! /usr/bin/env node
+
 const { commands, parseArgs } = require("./src/cli.js");
 
-// Uses the argv Object to detect 
+// Uses the argv Object to detect
 // if file gets called from cli or gets required from somewhere
-
+console.log("start");
 if (process.argv[1].endsWith("index.js")) {
   var args = parseArgs();
   var command = commands[args.command];
